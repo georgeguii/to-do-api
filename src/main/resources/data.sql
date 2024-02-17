bbc656b5f4a5233270c7e4774cea53fb7a99c4af
George Guilherme <george.gui2000@gmail.com>
2024-02-17 18:48:35 -0300

    feat: config H2 database and seed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tasks (id, title, description, status, created_at) VALUES
+('f47ac10b-58cc-4372-a567-0e02b2c3d479', 'Tarefa 1', 'Descrição da tarefa 1',0, '2024-02-14 12:01:52'),
+('1e2b1b4d-1b3c-4d3e-8a6f-1c3d2e1f0a1b', 'Tarefa 2', 'Descrição da tarefa 2', 0, '2024-02-15 17:27:35'),
+('2e3b4c5d-4e5f-6a7b-8c9d-0e1f2a3b4c5d', 'Tarefa 3', 'Descrição da tarefa 3', 1, '2024-02-17 03:04:56');
